841d310d0c019c9d995a740589c51c538d75302d
Selvakumar G <65029376+gselva28@users.noreply.github.com>
2020-07-21 21:09:04 +0530

    Add files via upload

---
+++ eq.py
@@ -0,0 +1,6 @@
+a=int(input("enter a value"))
+b=int(input("enter b value"))
+if a==b:
+    print("true")
+else:
+  print("false")
---
+++ listcmp.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  3 21:19:59 2019
+
+@author: SELVAKUMAR
+"""
+
+number_list=[x for x in range(20) if x % 2== 0]
+print(number_list)
